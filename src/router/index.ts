6f2271c1151c246b035ede71d1b91e61f9fa2dbe
Freedy <21914103@zju.edu.cn>
2020-09-03 14:57:27 +0800

    创建三个页面

---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Money from '@/views/Money.vue'
+import Labels from '@/views/Labels.vue'
+import Statistic from '@/views/Statistic.vue'
+
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    redirect: '/money'
+  },
+  {
+    path: '/money',
+    component: Money
+  },
+  {
+    path: '/labels',
+    component: Labels
+  },
+  {
+    path: '/statistic',
+    component: Statistic
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
