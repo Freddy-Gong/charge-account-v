5e1af0b4341d5994831b3715f6c995f2f9e51cfb
Freedy <21914103@zju.edu.cn>
2020-09-06 16:05:06 +0800

    用TS写一个组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -7,25 +7,37 @@
   </div>
 </template>
 
-<script lang="js">
-export default {
-  name: "Types",
-  props:['xxx'],
-  data(){
-      return{
-          type:'-'
-      }
-  },
-  methods:{
-      selectType(type){
-          this.type = type
-      }
-  },
-  mounted(){
-      console.log(this.xxx)
-  }
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
 
-};
+@Component
+export default class Types extends Vue {
+  type: "-" | "+" = "-";
+  @Prop(Number) xxx: number | undefined; //左边是传值的类型，右边的是对其操作的时候的检查
+  //左边告诉Vue 运行时 右边告诉TS 编译时
+  selectType(type: "-" | "+") {
+    this.type = type;
+  }
+}
+// export default {
+//   name: "Types",
+//   props:['xxx'],
+//   data(){
+//       return{
+//           type:'-'
+//       }
+//   },
+//   methods:{
+//       selectType(type){
+//           this.type = type
+//       }
+//   },
+//   mounted(){
+//       console.log(this.xxx)
+//   }
+
+// };
 </script>
 
 <style lang="scss" scoped>
