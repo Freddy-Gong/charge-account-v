dc91a98adc1cd72a823f0e5a4ee03f904184613b
Freedy <21914103@zju.edu.cn>
2020-09-06 14:09:48 +0800

    添加全局变量

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,15 @@
 
 
 <style lang="scss">
+body {
+  line-height: 1.5;
+  font-family: -apple-system, "Noto Sans", "Helvetica Neue", Helvetica,
+    "Nimbus Sans L", Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB",
+    "Noto Sans CJK SC", "Source Han Sans SC", "Source Han Sans CN",
+    "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti",
+    SimHei, "WenQuanYi Zen Hei Sharp", sans-serif;
+  color: #333;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
